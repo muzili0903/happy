b34fbf7dd342012b821267f444d7d727966fb1c1
muzili0903 <2584167983@qq.com>
2021-01-19 22:01:36 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './pages/home/Home'
+export default {
+  name: 'App',
+  components: {Home}
+}
+</script>
+
+<style>
+
+</style>
