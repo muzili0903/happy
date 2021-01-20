75d30dd66b1be10f26ee8827b82ab39527935c88
muzili0903 <2584167983@qq.com>
2021-01-20 21:48:52 +0800

    add header

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      // 修改path路径别名
+      'styles': resolve('src/assets/styles'),
     }
   },
   module: {
