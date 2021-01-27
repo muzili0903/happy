86cd1165fd5eb165163033f7145b8af810bb1a21
muzili0903 <2584167983@qq.com>
2021-01-27 21:07:11 +0800

    add icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <swiper :options="swiperOptions">
       <swiper-slide v-for="item of swiperList" :key=item.id>
-        <img class="swiper-img" :src="item.imgeUrl"/>
+        <img class="swiper-img" :src="item.imgUrl"/>
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper>
@@ -21,11 +21,11 @@ export default {
       },
       swiperList: [{
         id: '0001',
-        imgeUrl: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=68285350,561487286&fm=26&gp=0.jpg'
+        imgUrl: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=68285350,561487286&fm=26&gp=0.jpg'
       },
       {
         id: '0002',
-        imgeUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2353279619,1248476231&fm=26&gp=0.jpg'
+        imgUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2353279619,1248476231&fm=26&gp=0.jpg'
       }]
     }
   }
