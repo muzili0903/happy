db6460b194337f1327910ba2ad50c9717ee339e9
muzili0903 <2584167983@qq.com>
2021-01-28 21:33:46 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="recommend-title">热门推销</div>
+    <ul>
+      <!-- li 引入 1像素边框 border-bottom-->
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl"/>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'https://gss0.baidu.com/70cFfyinKgQFm2e88IuM_a/forum/w=580/sign=9489188db3de9c82a665f9875c8080d2/fd039245d688d43f81456d467d1ed21b0ff43b91.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首战，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'https://gss0.baidu.com/70cFfyinKgQFm2e88IuM_a/forum/w=580/sign=9489188db3de9c82a665f9875c8080d2/fd039245d688d43f81456d467d1ed21b0ff43b91.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首战，浪漫的海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'https://gss0.baidu.com/70cFfyinKgQFm2e88IuM_a/forum/w=580/sign=9489188db3de9c82a665f9875c8080d2/fd039245d688d43f81456d467d1ed21b0ff43b91.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首战，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .recommend-title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      // 图片定宽定高
+      width 1.7rem
+      height 1.7rem
+      // 上下左右都是 .1rem
+      padding .1rem
+    .item-info
+      // flex 1 自动撑开屏幕的宽度
+      flex 1
+      padding .1rem
+      // ellipsis() 没有显示 ... 需要加上 min-width 0
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        ellipsis()
+      .item-button
+        // 调整 button 的高度
+        line-height .44rem
+        margin-top .16rem
+        background #ff9300
+        // 上下 0rem 左右 .2rem
+        padding 0 .2rem
+        // 给border 一个圆角
+        border-radius .06rem
+        color #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li v-for="item of weekendList" :key="item.id">
+        <div class="weekend-img-wrapper">
+          <img class="weekend-img" :src="item.imgUrl"/>
+        </div>
+        <div class="weekend-info">
+          <p class="weekend-title">{{item.title}}</p>
+          <p class="weekend-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '001',
+        imgUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4161958695,1981548690&fm=15&gp=0.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首战，浪漫的海洋主题乐园'
+      }, {
+        id: '002',
+        imgUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage.photophoto.cn%2Fnm-23%2F036%2F003%2F0360030791.jpg&refer=http%3A%2F%2Fimage.photophoto.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614432597&t=d8b0d37b63d4d81b0a1d4f5102ffb704',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首战，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .weekend-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 33.9%
+    .weekend-img
+      width 100%
+  .weekend-info
+    padding 0.1rem
+    .weekend-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .weekend-desc
+      line-height .54rem
+      font-size .32rem
+      color #ccc
+      ellipsis()
+</style>
