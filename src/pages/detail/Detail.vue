7c6a7d67b3388b01bc4bafa5e51c958249131953
muzili0903 <2584167983@qq.com>
2021-03-26 21:28:00 +0800

    add detail header opacity

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,25 @@
 <template>
     <div>
       <detail-banner></detail-banner>
+      <detail-header></detail-header>
+      <!--为了让页面可以滚动，添加 div -->
+      <div class="content"></div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height 50rem
 </style>
